bcb3a92176597343ce2a8000c455d8094f557c7a
caohongjie <727980087@qq.com>
2021-10-18 21:30:07 +0800

    修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,13 @@
 <template>
-	<div class="wrap">
-		home
+	<div class="home">
+		<h1>网站快速成型工具</h1>
+		<p>
+			Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0
+			的桌面端组件库
+		</p>
+		<div class="img-wrap">
+			<img src="@/assets/img/home.png" alt="home" />
+		</div>
 	</div>
 </template>
 
@@ -14,7 +21,27 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.wrap {
-	font-size: 30px;
+.home {
+	text-align: center;
+	h1 {
+		font-size: 34px;
+		margin: 0;
+		line-height: 48px;
+		color: #555;
+	}
+	p {
+		font-size: 18px;
+		line-height: 28px;
+		color: #888;
+		margin: 10px 0 5px;
+	}
+	.img-wrap {
+		width: 890px;
+		margin: 30px auto;
+		position: relative;
+		img {
+			width: 100%;
+		}
+	}
 }
 </style>
