304183297ea37ca2288c636940aecdd0ec52fee5
曹鸿杰 <727980087@qq.com>
2021-10-18 17:50:02 +0800

    header修改

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,36 +1,42 @@
 <template>
-	<div class="wrap">
-		<el-container>
-			<el-header>
-				<div class="header">
-					Header
-				</div>
-			</el-header>
-			<el-main>
-				<keep-alive>
-					<router-view v-if="$route.meta.cache"></router-view>
-				</keep-alive>
-				<router-view v-if="!$route.meta.cache"></router-view>
-			</el-main>
-		</el-container>
-	</div>
+  <div class="wrap">
+    <el-container>
+      <el-header class="header-wrap">
+        <Header />
+      </el-header>
+      <el-main class="main-wrap">
+        <keep-alive>
+          <router-view v-if="$route.meta.cache"></router-view>
+        </keep-alive>
+        <router-view v-if="!$route.meta.cache"></router-view>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import Header from "./header.vue"
 export default {
-	name: 'Home',
-	data() {
-		return {}
-	},
+  name: 'Home',
+  components: {
+    Header
+  },
+  data() {
+    return {}
+  },
 }
 </script>
 
 <style lang="less" scoped>
 .wrap {
-	font-size: 30px;
-	.header {
-		background: rgb(0, 195, 255);
-		height: 100%;
-	}
+  font-size: 30px;
+  .header-wrap {
+    height: 80px !important;
+  }
+  .main-wrap {
+    min-height: calc(100vh - 80px);
+    width: 1140px;
+    margin: 0 auto;
+  }
 }
 </style>
